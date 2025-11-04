bb1c3bda2c6d4db4c5c4fb9c23a96ab5e09f4fcb
SadikHasanRafi <sadikk.hasan@gmail.com>
2025-11-04 18:24:50 +0600

    test1

--- src/components/sections/about/About.jsx
+++ src/components/sections/about/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
     <section id="about" className="min-h-screen flex items-center justify-center py-20 ">
       <RevealOnScroll>
         <div className="max-w-3xl mx-auto px-4 ">
-          <h2 className="text-3xl mb-8 font-bold bg-gradient-to-r from-blue-500  to-cyan-400 bg-clip-text text-center text-transparent ">About Me</h2>
+          <h2 className="text-3xl mb-8 font-bold bg-gradient-to-r from-blue-500  to-cyan-400 bg-clip-text text-center text-transparent ">About  Me</h2>
 
           <div className=" rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
             <p className="text-gray-300 mb-6">Driven by curiosity and a love for building, I craft digital experiences that merge clean code with creative vision - turning ideas into impactful, scalable solutions.</p>
